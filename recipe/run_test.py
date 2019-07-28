8b602c851b1ce16655941e27da9bbdc5c227f7c7
Patrick Sodré <psodre@gmail.com>
2019-07-27 23:10:29 -0400

    Change tmpdir

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,3 +1,8 @@
+import os
 import tabula
+
+# Windows automated builds are failing because of the TMPDIR location
+os.environ['TMPDIR'] = os.getcwd()
+
 # Read remote pdf into DataFrame
 df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")

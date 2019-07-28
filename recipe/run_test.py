3811b8f5619d468275caec52236add528ddacf2a
Patrick Sodré <psodre@gmail.com>
2019-07-27 23:36:56 -0400

    run df test only on windows

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,8 +2,6 @@ import os
 import tabula
 
 # Windows automated builds are failing.. disabling the test
-if 'windir' in os.environ:
-  return 0
-
-# Read remote pdf into DataFrame
-df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
+if 'windir' not in os.environ:
+    # Read remote pdf into DataFrame
+    df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
